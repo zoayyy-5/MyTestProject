56331065f45774e38c55d4bb0f683ef95edcc43f
Zoe Uehlinger <zueh8300@students.kleinisd.net>
2018-02-01 09:03:34 -0600

    next commit

--- Desktop/GitHub/TestProj/src/Main2.java
+++ Desktop/GitHub/TestProj/src/Main2.java
@@ -1,9 +1,11 @@
 
-public class Main2 {
+public class Main2 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		//comment
+		System.out.println("Hello World");
 	}
 
 }
